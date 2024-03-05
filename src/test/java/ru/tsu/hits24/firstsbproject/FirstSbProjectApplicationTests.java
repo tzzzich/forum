ff3878f0a0a405b5ef99a0912b5bbfe26ac73183
Viktoriya Bukharova <bvv005tkun@gmail.com>
2024-03-05 21:35:58 +0700

    chore: initial commit
    Added initial project setup

---
+++ src/test/java/ru/tsu/hits24/firstsbproject/FirstSbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits24.firstsbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
